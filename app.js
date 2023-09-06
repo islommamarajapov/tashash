311d66b2fba7d48cba155b5d9ee674d2858e2078
islom2008 <islommamarajapov2008@gmail.com>
2023-09-06 14:03:57 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// const firstElement=document.body.children[0]
+// console.log(firstElement);
+// children childNodes farqi children HTMLCollection qaytaradi. 
+// HTMLCollection bu aniq bir elementlar qatoriga joy olgan aniq bir elementlarni target qiladi.
+// Nodelist esa aniq bir elementlarga tutash bolgan yashirin elementlarni chiqarishni taklif qiladi.
+// const bodyElement=document.body;
+// console.log(bodyElement.children);
+// console.log(bodyElement.childNodes);
+
+
+// const firstItem=document.body.children[0];
+// console.log(firstItem);
+// const firstItemChild=firstItem.children[0];
+// console.log(firstItemChild.hasChildNodes());
+
